dec135f406b48a21ee4a8d2afc75f5e2ee7b44b0
zotov228 <zotov228@tut.by>
2016-10-26 16:57:04 +0300

    ppap
    Jobs

---
+++ js/scroll.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	  $('.bxslider').bxSlider({
+	  auto: true,
+	  autoControls: true
+	});
+});
---
+++ php/mail.php
@@ -0,0 +1,77 @@
+<? 
+// ----------------------------конфигурация-------------------------- // 
+ 
+$adminemail="webrent@tut.by";  // e-mail админа 
+$date=date("d.m.y"); // число.месяц.год 
+$time=date("H:i"); // часы:минуты:секунды 
+$backurl="http://ppap.by/";  // На какую страничку переходит после отправки письма 
+ 
+//---------------------------------------------------------------------- // 
+ 
+  
+ 
+// Принимаем данные с формы 
+ 
+$name=$_POST['name']; 
+$email=$_POST['email']; 
+$msg=$_POST['message']; 
+ 
+  
+ 
+// Проверяем валидность e-mail 
+ 
+if (!preg_match("|^([a-z0-9_\.\-]{1,20})@([a-z0-9\.\-]{1,20})\.([a-z]{2,4})|is", 
+strtolower($email))) 
+ 
+ { 
+ header('Content-type: text/html; charset=utf-8');
+  echo 
+"<center>Вернитесь <a 
+href='javascript:history.back(1)'><B>назад</B></a>. Вы 
+указали неверные данные!"; 
+ 
+  } 
+ 
+ else 
+ 
+ { 
+ 
+ 
+$msg=" 
+<p>Имя: $name</p> 
+<p>E-mail: $email</p> 
+<p>Сообщение: $msg</p> 
+"; 
+ 
+  
+ 
+ // Отправляем письмо админу  
+ 
+mail("$adminemail", "$date $time Сообщение 
+от $name", "$msg"); 
+ 
+  
+ 
+// Сохраняем в базу данных 
+ 
+$f = fopen("message.txt", "a+"); 
+ 
+fwrite($f," \n $date $time Сообщение от $name"); 
+fwrite($f,"\n $msg "); 
+fwrite($f,"\n ---------------"); 
+fclose($f); 
+ 
+  
+ 
+// Выводим сообщение пользователю 
+header('Content-type: text/html; charset=utf-8'); 
+print "<script language='Javascript'><!-- 
+function reload() {location = \"$backurl\"}; setTimeout('reload()', 6000); 
+//--></script> 
+ 
+$msg 
+<p>Сообщение отправлено! Подождите, сейчас вы будете перенаправлены на главную страницу...</p>";  
+exit; 
+ } 
+ 
+?>
